ffb8644e042d81dced0f6ce467b35aa01cdbcfc4
Nanxi Liu <nanxi@mit.edu>
2018-07-08 15:38:07 -0700

    Finished lecture 18 - manual data binding... learned about virtual DOM and made counter buttons work

--- src/app.js
+++ src/app.js
@@ -20,30 +20,32 @@ const template = (
 
 let count = 0;
 const addOne = () => {
-    console.log('addOne')
+    count++;
+    renderCounterApp();
 };
 
-// Challenge
-// Make button "-1" setup minusOne function and register - log "minusOne"
-// Make reset button "reset" - setup reset function - log "reset"
-
 const minusOne = () => {
-    console.log('minusOne')
+    count--;
+    renderCounterApp();
 };
 
 const reset = () => {
-    console.log('reset')
+    count = 0;
+    renderCounterApp();
 }
-
-const templateTwo = (
-    <div>
-        <h1>Count: {count} </h1>
-        <button onClick={addOne}>+1</button>
-        <button onClick={minusOne}>-1</button>
-        <button onClick={reset}>Reset</button>
-    </div>
-);
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+const renderCounterApp = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count} </h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minusOne}>-1</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    );
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
