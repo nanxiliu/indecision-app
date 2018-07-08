33faf032eff37b99174b5749b66b47732a319103
Nanxi Liu <nanxi@mit.edu>
2018-07-08 16:15:36 -0700

    Finished lecture 20 - picking an option... Added make decision button to randomly select an option, finished basic indecision app functionality

--- src/app.js
+++ src/app.js
@@ -14,24 +14,31 @@ const onFormSubmit = (e) => { // e = event object, e contains various info about
     if (option) {
         app.options.push(option); // add option to options array
         e.target.elements.option.value = ''; // reset to blank
-        renderOptions();
+        render();
     }
 }
 
-const removeAll = () => {
+const onRemoveAll = () => {
     app.options = [];
-    renderOptions();
+    render();
+}
+
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
 }
 
 const appRoot = document.getElementById('app');
 
-const renderOptions = () => {
+const render = () => {
     const template = (
         <div> 
             <h1>{app.title}</h1> 
             {app.subtitle && <p>{app.subtitle}</p>}
             <p>{app.options.length > 0 ? 'Here are your options:' : 'No options available!'}</p>
-            <button onClick = {removeAll}>Remove All</button>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+            <button onClick = {onRemoveAll}>Remove All</button>
             <ol>
                 {
                     app.options.map((option) => <li key={option}>{option}</li>)
@@ -46,4 +53,4 @@ const renderOptions = () => {
     ReactDOM.render(template, appRoot);
 }
 
-renderOptions();
+render();
