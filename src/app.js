bd49336de4c5a49debfd8a21e30014fc010114e2
Nanxi Liu <nanxi@mit.edu>
2018-07-08 20:52:44 -0700

    Finished lecture 37 - indecision state: part II

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ class IndecisionApp extends React.Component {
         super(props);
         this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
         this.handlePick = this.handlePick.bind(this);
+        this.handleAddOption = this.handleAddOption.bind(this);
         this.state = {
-            options: ['hi', 'oh', 'yo']
+            options: []
         };
     }
 
@@ -22,6 +23,20 @@ class IndecisionApp extends React.Component {
         alert(option);
     }
 
+    handleAddOption(option) {
+        if (!option) { // if option is empty
+            return 'Enter a valid, non-empty value to add an item.';
+        } else if (this.state.options.indexOf(option) > -1) { // index would only be > -1 if it was in array
+            return 'This option already exists.'; 
+        } 
+
+        this.setState((prevState) => {
+            return {
+                options: prevState.options.concat(option)
+            }
+        });
+    }
+
     render() {
         const title = 'Indecision';
         const subtitle = 'Put your life in the hands of a computer.';
@@ -37,7 +52,9 @@ class IndecisionApp extends React.Component {
                     options={this.state.options}
                     handleDeleteOptions={this.handleDeleteOptions}
                 />
-                <AddOption />
+                <AddOption 
+                    handleAddOption={this.handleAddOption}
+                />
             </div>
         );
     }
@@ -94,19 +111,29 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.state = {
+            error: undefined // default = no error... if error, state is updated
+        }
+    }
+
     handleAddOption(e) {
         e.preventDefault();
 
         const option = e.target.elements.option.value.trim(); // trim deletes leading and ending blank spaces
-
-        if (option){
-            alert(option);
-        }
+        const error =  this.props.handleAddOption(option); // only get a return value if there's an error... if not, just does handleAddOption
+    
+        this.setState(() => { 
+            return { error };
+        });
     }
 
     render() {
         return (
             <div> 
+                {this.state.error && <p>{this.state.error} </p>}
                 <form onSubmit={this.handleAddOption}>
                     <input type="text" name="option" />
                     <button>Add Option</button>
