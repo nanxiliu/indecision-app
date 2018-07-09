12fc37a5c5565ddb9347a49cabce91c3417d4239
Nanxi Liu <nanxi@mit.edu>
2018-07-09 10:36:46 -0700

    Finished Lecture 51 - ES6 import/export... learned how to use named exports to import functions from separate files

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+console.log('utils.js is running');
+
+export const square = (x) => x * x;
+export const add = (a, b) => a + b;
+
+// named exports 
+// do this is if you don't put export in front of the function
+// export { square, add };
