e6b7d5259e369d9e499bb8743a616540cb796fd1
Nanxi Liu <nanxi@mit.edu>
2018-07-09 11:18:57 -0700

    Finished Lecture 53 - Importing npm modules... imported React and React DOM and showed importing validator

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
-import subtract, { square, add } from './utils.js';
-import isSenior, { isAdult, canDrink } from './person.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js is running');
+const template = React.createElement('p', {}, 'testing');
+ReactDOM.render(template, document.getElementById('app'));
 
-// from utils.js
-console.log(square(4));
-console.log(add(100, 3));
-console.log(subtract(20, 3));
+// import validator from 'validator';
+// console.log(validator.isEmail('test@gmail.com'));
 
-// person.js
-console.log(isAdult(18));
-console.log(canDrink(18));
-console.log(isSenior(68));
+// import subtract, { square, add } from './utils.js';
+// import isSenior, { isAdult, canDrink } from './person.js';
+
+// console.log('app.js is running');
+
+// // from utils.js
+// console.log(square(4));
+// console.log(add(100, 3));
+// console.log(subtract(20, 3));
+
+// // person.js
+// console.log(isAdult(18));
+// console.log(canDrink(18));
+// console.log(isSenior(68));
