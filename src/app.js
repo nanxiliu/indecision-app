05cf28be9c01726abcbba031c0cd2d8ded387917
Nanxi Liu <nanxi@mit.edu>
2018-07-08 18:32:15 -0700

    Finished lecture 28 - component props

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 class IndecisionApp extends React.Component {
     render() {
+        const title = 'Indecision';
+        const subtitle = 'Put your life in the hands of a computer.';
+        const options = ['Thing one', 'Thing two', 'Thing three'];
         return (
             <div> 
-                <Header />
+                <Header title = {title} subtitle = {subtitle}/>
                 <Action />
-                <Options />
+                <Options options = {options}/>
                 <AddOption />
             </div>
         );
@@ -16,8 +19,8 @@ class Header extends React.Component {
     render() {
         return (
             <div> 
-                <h1>Indecision </h1>
-                <h2>Put your life in the hands of a computer.</h2>
+                <h1>{this.props.title} </h1>
+                <h2>{this.props.subtitle}</h2>
             </div>
         );
     }
@@ -37,8 +40,9 @@ class Options extends React.Component {
     render() {
         return (
             <div> 
-                <p>Options here! </p>
-                <Option />
+                {
+                    this.props.options.map((option) => <Option key={option} optionText={option} />)
+                }
             </div>
         );
     }
@@ -48,7 +52,7 @@ class Option extends React.Component {
     render() {
         return (
             <div> 
-                <p>Individual option! </p>
+                <p>Option: {this.props.optionText} </p>
             </div>
         );
     }
