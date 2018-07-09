0a7cb5720144fb522a4041df95d6a967b7d328b8
Nanxi Liu <nanxi@mit.edu>
2018-07-08 21:14:28 -0700

    Finished lecture 41 - default props

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-//stateless functional component
-
 class IndecisionApp extends React.Component {
     constructor(props) {
         super(props);
@@ -7,7 +5,7 @@ class IndecisionApp extends React.Component {
         this.handlePick = this.handlePick.bind(this);
         this.handleAddOption = this.handleAddOption.bind(this);
         this.state = {
-            options: []
+            options: props.options
         };
     }
 
@@ -40,12 +38,11 @@ class IndecisionApp extends React.Component {
     }
 
     render() {
-        const title = 'Indecision';
         const subtitle = 'Put your life in the hands of a computer.';
 
         return (
             <div> 
-                <Header title={title} subtitle={subtitle}/>
+                <Header subtitle={subtitle}/>
                 <Action 
                     hasOptions={this.state.options.length > 0}
                     handlePick={this.handlePick}
@@ -62,13 +59,21 @@ class IndecisionApp extends React.Component {
     }
 }
 
+IndecisionApp.defaultProps = {
+    options: []
+}
+
 const Header = (props) => {
     return (
         <div> 
             <h1>{props.title} </h1>
-            <h2>{props.subtitle}</h2>
+            {props.subtitle && <h2>{props.subtitle}</h2>}
         </div>
     );
+};
+
+Header.defaultProps = {
+    title: 'Indecision'
 };
 
 const Action = (props) => {
@@ -136,14 +141,4 @@ class AddOption extends React.Component {
     }
 }
 
-// Sample stateless functional component
-// const User = (props) => {
-//     return (
-//         <div>
-//             <p>Name: {props.name} </p>
-//             <p>Age: {props.age}</p>
-//         </div>
-//     );
-// };
-
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp options={['nanxi', 'eric', 'woo']}/>, document.getElementById('app'));
