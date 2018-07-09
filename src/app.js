65975c148216674f603a9d24615b1cd19a24c279
Nanxi Liu <nanxi@mit.edu>
2018-07-09 14:27:09 -0700

    Finished Lecture 60 - Passing Children to Component - can pass JSX to components and use props.children

--- src/app.js
+++ src/app.js
@@ -2,33 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp'
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const Layout = (props) => {
+    return (
+        <div>
+            <p>Header</p>
+            {props.children}
+            <p>Footer</p>
+        </div>
+    );
+};
 
-class OldSyntax {
-    constructor() {
-        this.name = 'Nanxi';
-        this.getGreeting = this.getGreeting.bind(this);
-    }
-    getGreeting() {
-        return `Hi. My name is ${this.name}.`;
-    }
-}
+const template = (
+    <div>
+        <h1>Page Title</h1>
+        <p>This is my page.</p>
+    </div>
+);
 
-const oldSyntax = new OldSyntax();
-const getGreeting = oldSyntax.getGreeting;
-console.log(getGreeting());
-
-// ----
-
-
-class NewSyntax {
-    name = 'Jen';
-    getGreeting = () => {
-        return `Hi. My name is ${this.name}.`;
-    }
-}
-
-const newSyntax = new NewSyntax();
-const newGetGreeting = newSyntax.getGreeting;
-console.log(newGetGreeting());
-console.log(newSyntax);
+ReactDOM.render((
+    <Layout>
+        <div>
+            <h1>Page Title</h1>
+            <p>This is my page.</p>
+        </div>
+    </Layout>
+), document.getElementById('app'));
