65b7360c38d1331d6caeee0da28e1be9e772bf10
Nanxi Liu <nanxi@mit.edu>
2018-07-08 17:03:27 -0700

    Finished lecture 25 - es6 classes part 2

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -12,10 +12,27 @@ class Person {
     }
 }
 
-const me = new Person('Nanxi Liu', 20);
-console.log(me.getGreeting());
-console.log(me.getDescription());
 
-const other = new Person();
-console.log(other.getGreeting());
-console.log(other.getDescription());
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age); // calls parent constructor function
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() { // override original method
+        let description = super.getDescription();
+        if (this.hasMajor()) {
+            description += ` His/her major is ${this.major}.`;
+        }
+        return description;
+    }
+}
+
+
+const me = new Student('Nanxi Liu', 20, 'Computer Science');
+console.log(me.getDescription())
+
+const other = new Student();
+console.log(other.getDescription())
