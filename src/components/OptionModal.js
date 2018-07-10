fc80ad34b866b66302d1d8384f785ad07882a2f8
Nanxi Liu <nanxi@mit.edu>
2018-07-10 00:52:28 -0700

    Finished Lecture 71 - Styling React-Modal... finished styling the modal, so everything works! Make sure to test for edge cases

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption}
         onRequestClose={props.handleClearSelectedOption} // clears the modal if you click the background
         contentLabel="Selected Option"
+        closeTimeoutMS={200}
+        className="modal"
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p> {props.selectedOption} </p>}
-        <button onClick={props.handleClearSelectedOption}>Clear</button>
+        <h3 className='modal__title'>Selected Option</h3>
+        {props.selectedOption && <p className='modal__body'> {props.selectedOption} </p>}
+        <button className='button' onClick={props.handleClearSelectedOption}>Clear</button>
     </Modal>
 );
 
